28e04eccedeb35218295e8b6bd5fd809d6939524
csengekulker <k71608883429@ktch.hu>
2022-10-24 11:42:54 +0200

    eventlistener and calc function without body

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 console.log("hellooo");
 
-const base_a = document.querySelector("#base_a");
-const base_c = document.querySelector("#base_c");
-const side_b = document.querySelector("#side_b");
-const side_d = document.querySelector("#side_d");
+const base_a = document.querySelector("#base_a") as HTMLInputElement;
+const base_c = document.querySelector("#base_c") as HTMLInputElement;
+const side_b = document.querySelector("#side_b") as HTMLInputElement;
+const side_d = document.querySelector("#side_d") as HTMLInputElement;
 
 const submitButton = document.querySelector("#submitButton");
 const resultField = document.querySelector("#resultField");
 
+function calcArea(a: number, b: number, c: number, d: number): number {
+    
+    return 3;
+}
+
+submitButton?.addEventListener('click', () => {
+    let a = Number(base_a.value);
+    let c = Number(base_c.value);
+
+    let b = Number(side_b.value);
+    let d = Number(side_d.value);
+
+    calcArea(a, b, c, d);
+})
