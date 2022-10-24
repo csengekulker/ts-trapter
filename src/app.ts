5d9e5dfd5b0f37847ae5bcc487cdc5a2cba9e106
csengekulker <k71608883429@ktch.hu>
2022-10-24 11:37:57 +0200

    declared constants

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
 console.log("hellooo");
+
+const base_a = document.querySelector("#base_a");
+const base_c = document.querySelector("#base_c");
+const side_b = document.querySelector("#side_b");
+const side_d = document.querySelector("#side_d");
+
+const submitButton = document.querySelector("#submitButton");
+const resultField = document.querySelector("#resultField");
+
