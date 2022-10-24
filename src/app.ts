2d45fbac8f5dd278235aaa7bbbed1b5fa867c445
csengekulker <k71608883429@ktch.hu>
2022-10-24 11:53:54 +0200

    result displayed in div

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,21 @@
-console.log("hellooo");
-
 const base_a = document.querySelector("#base_a") as HTMLInputElement;
 const base_c = document.querySelector("#base_c") as HTMLInputElement;
 const side_b = document.querySelector("#side_b") as HTMLInputElement;
 const side_d = document.querySelector("#side_d") as HTMLInputElement;
 
 const submitButton = document.querySelector("#submitButton");
-const resultField = document.querySelector("#resultField");
+const resultField = document.querySelector("#resultField") as HTMLInputElement;
 
-function calcArea(a: number, b: number, c: number, d: number): number {
+function calcArea(a: number, b: number, c: number, d: number) {
 
     let underSqrt = (a+b-c+d) * (a-b-c+d) * (a+b-c-d) * (b+c+d-a)
 
     let next = Math.sqrt(underSqrt);
 
-
     let result = (a + c) * next / (4 * (a - c))
 
-    console.log(result);
+    resultField.innerHTML = result.toLocaleString();
     
-    return result;
 }
 
 submitButton?.addEventListener('click', () => {
